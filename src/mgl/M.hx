45ac84da5434c7bc6702adb81e4f55f501e06632
ABA Games <cs8k-cyu@asahi-net.or.jp>
2013-05-06 21:14:27 +0900

    The position is divided by the screen size.

--- src/mgl/M.hx
+++ src/mgl/M.hx
@@ -2,12 +2,17 @@ package mgl;
 import flash.display.Sprite;
 import flash.events.Event;
 import flash.events.MouseEvent;
-import flash.Lib;
 class M { // Mouse
 	public var p:V; // pos
 	public var ip = false; // isPressing
 
-	public function new(baseSprite:Sprite):Void {
+	static var screenSize:V;
+	static var baseSprite:Sprite;
+	public static function initialize(game:G) {
+		screenSize = game.screenSize;
+		baseSprite = game.baseSprite;
+	}
+	public function new():Void {
 		p = new V();
 		baseSprite.addEventListener(MouseEvent.MOUSE_MOVE, onMoved);
 		baseSprite.addEventListener(MouseEvent.MOUSE_DOWN, onPressed);
@@ -15,8 +20,8 @@ class M { // Mouse
 		baseSprite.addEventListener(Event.MOUSE_LEAVE, onReleased);
 	}
 	function onMoved(e:MouseEvent):Void {
-		p.x = e.stageX;
-		p.y = e.stageY;
+		p.x = e.stageX / screenSize.x;
+		p.y = e.stageY / screenSize.y;
 	}
 	function onPressed(e:MouseEvent):Void {
 		ip = true;
