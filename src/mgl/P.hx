309f35de1847a4dfe81f5d73eff1f5cc33e87ea8
ABA Games <cs8k-cyu@asahi-net.or.jp>
2013-11-27 22:35:43 +0900

    Add the scrolling method

--- src/mgl/P.hx
+++ src/mgl/P.hx
@@ -1,22 +1,28 @@
 package mgl;
+import mgl.G.Screen;
 class P { // Particle
 	static public var i(get, null):P; // instance
+	static public function sc(vx:Float, vy:Float = 0,
+	minX:Float = -9999999, maxX:Float = 9999999, minY:Float = -9999999, maxY:Float = 9999999):Void {
+		A.sc("mgl.PActor", vx, vy, minX, maxX, minY, maxY);
+	}
 	public function p(pos:V):P { return setPos(pos); }
 	public function xy(x:Float, y:Float):P { return setXy(x, y); }
+	public function z(z:Float = 0):P { return setZ(z); }
 	public function c(color:C):P { return setColor(color); }
 	public function cn(count:Int):P { return setCount(count); }
 	public function sz(size:Float):P { return setSize(size); }
 	public function s(speed:Float):P { return setSpeed(speed); }
 	public function t(ticks:Float):P { return setTicks(ticks); }
-	public function an(angle:Float, angleWidth:Float):P { return setAngle(angle, angleWidth); }
+	public function w(angle:Float, angleWidth:Float = 0.0):P { return setWay(angle, angleWidth); }
 	public var a(get, null):P; // add
 
 	static var random:R;
 	public static function initialize():Void {
 		random = new R();
 	}
-	public var actor:PActor;
 	var aPos:V;
+	var aZ = 0.0;
 	var aColor:C;
 	var aCount = 1;
 	var aSize = 0.02;
@@ -24,8 +30,7 @@ class P { // Particle
 	var aTicks = 60;
 	var aAngle = 0.0;
 	var aAngleWidth = 360.0;
-	public function new(createActor:Bool = false) {
-		if (createActor) actor = new PActor();
+	public function new() {
 		aPos = new V();
 		aColor = C.wi;
 	}
@@ -38,6 +43,10 @@ class P { // Particle
 	}
 	function setXy(x:Float, y:Float):P {
 		aPos.xy(x, y);
+		return this;
+	}
+	function setZ(z:Float):P {
+		aZ = z;
 		return this;
 	}
 	function setColor(color:C):P {
@@ -60,27 +69,30 @@ class P { // Particle
 		aTicks = Std.int(ticks);
 		return this;
 	}
-	function setAngle(angle:Float, angleWidth:Float):P {
+	function setWay(angle:Float, angleWidth:Float):P {
 		aAngle = angle;
 		aAngleWidth = angleWidth;
 		return this;
 	}
 	function get_a():P {
 		for (i in 0...aCount) {
-			var p = new P(true);
-			p.actor.color = aColor;
-			p.actor.targetSize = aSize;
-			p.actor.p.v(aPos);
-			p.actor.v.aa(aAngle + random.p(aAngleWidth / 2), random.n(aSpeed));
-			p.actor.removeTicks = Std.int(aTicks * random.f(0.5, 1.5));
+			var pa = new PActor();
+			pa.color = aColor;
+			pa.targetSize = aSize;
+			pa.p.v(aPos);
+			pa.z = aZ;
+			pa.v.aw(aAngle + random.p(aAngleWidth / 2), random.n(aSpeed));
+			pa.removeTicks = Std.int(aTicks * random.f(0.5, 1.5));
 		}
 		return this;
 	}
 }
 class PActor extends A {
 	static var pixelSize:V;
+	static var pixelSizeL:Float;
 	override public function i() {
-		pixelSize = B.pixelSize;
+		pixelSize = G.pixelSize;
+		pixelSizeL = G.pixelWHRatio < 1 ? pixelSize.x : pixelSize.y;
 		dp(0);
 	}
 	public var removeTicks = 60;
@@ -97,11 +109,22 @@ class PActor extends A {
 		} else {
 			size *= 0.95;
 		}
-		var s = size * pixelSize.x;
-		var px = Std.int(p.x * pixelSize.x) - Std.int(s / 2);
-		var py = Std.int(p.y * pixelSize.y) - Std.int(s / 2);
-		var ps = Std.int(s);
-		B.pixelFillRect(px, py, ps, ps, color.getBlinkColor());
-		if (ticks > removeTicks) r;
+		var s = size * pixelSizeL;
+		var px:Int;
+		var py:Int;
+		var ps:Int;
+		if (z == 0) {
+			px = Std.int(p.x * pixelSize.x - s / 2);
+			py = Std.int(p.y * pixelSize.y - s / 2);
+			ps = Std.int(s);
+		} else {
+			var zs = 1.0 / (z + 1);
+			var szs = s * zs;
+			px = Std.int(((p.x - 0.5) * zs + 0.5) * pixelSize.x - szs / 2);
+			py = Std.int(((p.y - 0.5) * zs + 0.5) * pixelSize.y - szs / 2);
+			ps = Std.int(szs);
+		}
+		Screen.pixelFillRect(px, py, ps, ps, color.getBlinkColor());
+		if (ticks >= removeTicks) get_r();
 	}
 }
