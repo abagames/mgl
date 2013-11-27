7dd892d848f3930bed6a44baca74afc04566a065
ABA Games <cs8k-cyu@asahi-net.or.jp>
2013-11-27 22:36:04 +0900

    Add the difficulty corrected random

--- src/mgl/R.hx
+++ src/mgl/R.hx
@@ -2,18 +2,26 @@ package mgl;
 class R { // Random
 	static public var i(get, null):R; // instance
 	public function n(v:Float = 1, s:Float = 0):Float { return get() * v + s; }	
-	public function ni(v:Int, s:Int = 0):Int { return Std.int(get() * (v + .999) + s); }	
+	public function ni(v:Int, s:Int = 0):Int { return Std.int(get() * (v + 0.999) + s); }	
 	public function f(from:Float, to:Float):Float { return get() * (to - from) + from; }
-	public function fi(from:Int, to:Int):Int { return Std.int(get() * (to - from + .999)) + from; }
+	public function fi(from:Int, to:Int):Int { return Std.int(get() * (to - from + 0.999)) + from; }
 	public var pm(get, null):Int; // plus minus
 	public function p(v:Float = 1):Float { return f(-v, v); }
 	public function pi(v:Int):Int { return fi(-v, v); }
 	public function s(v:Int = -0x7fffffff):R { return setSeed(v); }
+	public function bst(stage:Int):Void { difficultyBasisStage = stage; }
+	public function st(stage:Int, seedOffset:Int = 0):R {
+		this.stage = stage;
+		return setSeed(stage + seedOffset);
+	}
+	public var dc(get, null):Float; // difficulty corrected random
 
 	var x = 0;
 	var y = 0;
 	var z = 0;
 	var w = 0;
+	var stage = 0;
+	var difficultyBasisStage = 100;
 	public function new() {
 		setSeed();
 	}
@@ -22,6 +30,9 @@ class R { // Random
 	}
 	function get_pm():Int {
 		return ni(1) * 2 - 1;
+	}
+	function get_dc():Float {
+		return Math.pow(n(), difficultyBasisStage / (stage + 1));
 	}
 
 	function setSeed(v:Int = -0x7fffffff):R {
