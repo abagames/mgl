f6451dda55edfab320f3b1b9b068ee38be73292b
ABA Games <cs8k-cyu@asahi-net.or.jp>
2013-11-27 22:30:56 +0900

    Add auto shape generation

--- src/mgl/D.hx
+++ src/mgl/D.hx
@@ -1,5 +1,8 @@
 package mgl;
+import mgl.G.Screen;
+import mgl.T.Letter;
 using Math;
+using mgl.U;
 class D { // DotShape
 	static public var i(get, null):D; // instance
 	public function sz(dotSize:Int):D { return setDotSize(dotSize); }
@@ -12,30 +15,34 @@ class D { // DotShape
 	}
 	public function st(threshold:Float):D { return setSpotThreshold(threshold); }
 	public function o(x:Float = 0, y:Float = 0):D { return setOffset(x, y); }
-	public function fr(width:Float, height:Float, edgeWidth:Int = 0):D {
+	public function fr(width:Float, height:Float = -1, edgeWidth:Int = 0):D {
 		return fillRect(width, height, edgeWidth);
 	}
-	public function lr(width:Float, height:Float, edgeWidth:Int = 1):D {
+	public function lr(width:Float, height:Float = -1, edgeWidth:Int = 1):D {
 		return lineRect(width, height, edgeWidth);
 	}
-	public function gr(width:Float, height:Float, color:C, seed:Int = -1):D {
-		return generateRect(width, height, color, seed);
+	public function gr(width:Float, height:Float = -1, seed:Int = -1):D {
+		return generateRect(width, height, seed);
 	}
-	public function fc(radius:Float, edgeWidth:Int = 0):D {
-		return fillCircle(radius, edgeWidth);
+	public function fc(diameter:Float, edgeWidth:Int = 0):D {
+		return fillCircle(diameter, edgeWidth);
 	}
-	public function lc(radius:Float, edgeWidth:Int = 1):D {
-		return lineCircle(radius, edgeWidth);
+	public function lc(diameter:Float, edgeWidth:Int = 1):D {
+		return lineCircle(diameter, edgeWidth);
 	}
-	public function gc(radius:Float, color:C, seed:Int = -1):D {
-		return generateCircle(radius, color, seed);
+	public function gc(diameter:Float, seed:Int = -1):D {
+		return generateCircle(diameter, seed);
+	}
+	public function gs(width:Float, height:Float = -1, seed:Int = -1):D {
+		return generateShape(width, height, seed);
 	}
 	public function tx(text:String):D {
 		return drawText(text);
 	}
 	public function p(pos:V):D { return setPos(pos); }
 	public function xy(x:Float, y:Float):D { return setXy(x, y); }
-	public function r(angle:Float):D { return rotate(angle); }
+	public function z(z:Float = 0):D { return setZ(z); }
+	public function rt(angle:Float = 0):D { return rotate(angle); }
 	public function sc(x:Float = 1, y:Float = -1):D { return setScale(x, y); }
 	public var ed(get, null):D; // enable dot scale
 	public var dd(get, null):D; // disable dot scale
@@ -43,15 +50,17 @@ class D { // DotShape
 	public function dc(color:C = null):D { return setDrawColor(color); }
 	public var d(get, null):D; // draw
 
+	static var baseRandomSeed = 0;
 	static var pixelSize:V;
 	static var pixelWHRatio:Float;
 	static var baseDotSize = 1;
 	static var rPos:V;
 	static var aVec:V;
-	public static function initialize() {
-		pixelSize = B.pixelSize;
-		pixelWHRatio = B.pixelWHRatio;
-		baseDotSize = B.baseDotSize;
+	public static function initialize(main:Dynamic) {
+		baseRandomSeed = U.ch(main);
+		pixelSize = G.pixelSize;
+		pixelWHRatio = G.pixelWHRatio;
+		baseDotSize = G.baseDotSize;
 		rPos = new V();
 		aVec = new V();
 	}
@@ -67,6 +76,7 @@ class D { // DotShape
 	var xySpotInterval = 0.0;
 	var offset:V;
 	var pos:V;
+	var pz = 0.0;
 	var angle = 0.0;
 	var scaleX = 1.0;
 	var scaleY = 1.0;
@@ -121,29 +131,108 @@ class D { // DotShape
 		return this;
 	}
 	function fillRect(width:Float, height:Float, edgeWidth:Int):D {
+		if (height < 0) height = width;
 		return setRect(width, height, edgeWidth, false);
 	}
 	function lineRect(width:Float, height:Float, edgeWidth:Int):D {
+		if (height < 0) height = width;
 		return setRect(width, height, edgeWidth, true);
 	}
-	function generateRect(width:Float, height:Float, color:C, seed:Int):D {
+	function generateRect(width:Float, height:Float, seed:Int):D {
+		if (height < 0) height = width;
+		var oc = C.di.v(color);
 		setGeneratedColors(color, seed);
-		fr(width, height).c(color).cb(color.gd).si().lr(width, height);
+		fr(width, height, 1).c(oc).cb(oc.gd).si().lr(width, height);
 		return this;
 	}
-	function fillCircle(radius:Float, edgeWidth:Int):D {
-		return setCircle(radius, edgeWidth, false);
+	function fillCircle(diameter:Float, edgeWidth:Int):D {
+		return setCircle(diameter, edgeWidth, false);
 	}
-	function lineCircle(radius:Float, edgeWidth:Int):D {
-		return setCircle(radius, edgeWidth, true);
+	function lineCircle(diameter:Float, edgeWidth:Int):D {
+		return setCircle(diameter, edgeWidth, true);
 	}
-	function generateCircle(radius:Float, color:C, seed:Int):D {
+	function generateCircle(diameter:Float, seed:Int):D {
+		var oc = C.di.v(color);
 		setGeneratedColors(color, seed);
-		fc(radius, 1).c(color).cb(color.gd).si().lc(radius);
+		fc(diameter, 1).c(oc).cb(oc.gd).si().lc(diameter);
+		return this;
+	}
+	function generateShape(width:Float, height:Float, seed:Int,
+		fillRatio:Float = 0.5, sideRatio:Float = 0.2, crossRatio:Float = 0.2):D {
+		if (height < 0) height = width;
+		if (seed < 0) seed = baseRandomSeed++;
+		var w = Std.int(width * pixelSize.x / dotSize / 2);
+		var h = Std.int(height * pixelSize.y / dotSize);
+		var oy = -Std.int(h / 2);
+		var pixels = new Array<Array<Int>>();
+		for (x in 0...w) {
+			var lp = new Array<Int>();
+			for (y in 0...h) lp.push(0);
+			pixels.push(lp);
+		}
+		var nextPixels = new Array<V>();
+		var oc = C.di.v(color);
+		setGeneratedColors(color, seed);
+		var r = R.i.s(seed);
+		for (i in 0...Std.int(w * h * fillRatio)) {
+			for (j in 0...100) {
+				if (nextPixels.length <= 0) nextPixels.push(V.i.xy(r.fi(2, w - 3), r.fi(2, h - 3)));
+				var np = nextPixels[r.ni(nextPixels.length - 1)];
+				var nx = np.xi, ny = np.yi;
+				nextPixels.remove(np);
+				if (pixels[nx][ny] > 0) continue;
+				var countSide = 0, countCross = 1;
+				for (ix in (nx - 1).ci(0, w - 1)...(nx + 2).ci(0, w)) {
+					for (iy in (ny - 1).ci(0, h - 1)...(ny + 2).ci(0, h)) {
+						if (pixels[ix][iy] > 0) {
+							if (ix == nx || iy == ny) countSide++;
+							else countCross++;
+						}
+					}
+				}
+				if (r.n() < 0.9 - countSide * sideRatio && r.n() < 0.9 - countCross * crossRatio) {
+					setDot(nx, ny + oy, ny / h);
+					setDot(-nx, ny + oy, ny / h);
+					pixels[nx][ny] = 1;
+					for (ix in (nx - 1).ci(0, w - 1)...(nx + 2).ci(0, w)) {
+						for (iy in (ny - 1).ci(0, h - 1)...(ny + 2).ci(0, h)) {
+							if (pixels[ix][iy] == 0) {
+								for (np in nextPixels) if (np.xi == ix && np.yi == iy) continue;
+								nextPixels.push(V.i.xy(ix, iy));
+							}
+						}
+					}
+					break;
+				}
+			}
+		}
+		for (x in 0...w) pixels[x][0] = pixels[x][h - 1] = 0;
+		for (y in 0...h) pixels[0][y] = pixels[w - 1][y] = 0;
+		c(oc).cb(oc.gd).si();
+		for (x in 1...w - 1) {
+			for (y in 1...h - 1) {
+				if (pixels[x][y] != 1) continue;
+				var hp = false;
+				for (ix in x - 1...x + 2) {
+					for (iy in y - 1...y + 2) {
+						if (pixels[ix][iy] == 0 && (ix == x || iy == y)) {
+							setDot(ix, iy + oy, iy / h);
+							setDot(-ix, iy + oy, iy / h);
+							pixels[ix][iy] = 2;
+						}
+					}
+				}
+			}
+		}
 		return this;
 	}
 	function drawText(text:String):D {
-		L.i.tx(text).avc.draw(pixelFillRect);
+		var l = new Letter();
+		l.setText(text);
+		l.alignCenter();
+		l.alignVerticalCenter();
+		l.setDotSize(1);
+		l.draw(pixelFillRect);
 		return this;
 	}
 	function setPos(pos:V):D {
@@ -152,6 +241,10 @@ class D { // DotShape
 	}
 	function setXy(x:Float, y:Float):D {
 		this.pos.xy(x, y);
+		return this;
+	}
+	function setZ(z:Float):D {
+		pz = z;
 		return this;
 	}
 	function rotate(angle:Float):D {
@@ -186,26 +279,34 @@ class D { // DotShape
 		return this;
 	}
 	function get_d():D {
+		var zs = 1.0 / (pz + 1);
+		var dox:Float;
+		var doy:Float;
+		var x:Float;
+		var y:Float;
+		if (pz == 0) {
+			dox = dotSize * scaleX;
+			doy = dotSize * scaleY;
+			x = pos.x * pixelSize.x;
+			y = pos.y * pixelSize.y;
+		} else {
+			dox = dotSize * scaleX * zs;
+			doy = dotSize * scaleY * zs;
+			x = ((pos.x - 0.5) * zs + 0.5) * pixelSize.x;
+			y = ((pos.y - 0.5) * zs + 0.5) * pixelSize.y;
+		}
 		var w:Float;
 		var h:Float;
 		if (isDotScale) {
-			w = dotSize * scaleX.abs();
-			h = dotSize * scaleY.abs();
+			w = dotSize * scaleX.abs() * zs;
+			h = dotSize * scaleY.abs() * zs;
 		} else {
 			w = h = dotSize;
 		}
-		w += .99;
-		h += .99;
-		var dox = dotSize * scaleX;
-		var doy = dotSize * scaleY;
-		/*if (isRollingShape) {
-			dox *= .7;
-			doy *= .7;
-		}*/
+		w += 0.99;
+		h += 0.99;
 		var rox = Std.int(w / 2);
 		var roy = Std.int(h / 2);
-		var x = pos.x * pixelSize.x;
-		var y = pos.y * pixelSize.y;
 		var pw:Int;
 		var ph:Int;
 		if (!isRollingShape) {
@@ -218,11 +319,11 @@ class D { // DotShape
 		for (d in dots) {
 			rPos.x = d.offset.x * dox;
 			rPos.y = d.offset.y * doy;
-			if (angle != 0) rPos.r(angle);
+			if (angle != 0) rPos.rt(angle);
 			var px = Std.int(x + rPos.x) - rox;
 			var py = Std.int(y + rPos.y) - roy;
-			if (drawColor != null) B.pixelFillRect(px, py, pw, ph, drawColor);
-			else B.pixelFillRect(px, py, pw, ph, d.color);
+			if (drawColor != null) Screen.pixelFillRect(px, py, pw, ph, drawColor);
+			else Screen.pixelFillRect(px, py, pw, ph, d.color);
 		}
 		return this;
 	}
@@ -243,9 +344,9 @@ class D { // DotShape
 		}
 		return this;
 	}
-	function setCircle(radius:Float, edgeWidth:Int, isDrawingEdge:Bool):D {
+	function setCircle(diameter:Float, edgeWidth:Int, isDrawingEdge:Bool):D {
 		var ps = pixelWHRatio < 1 ? pixelSize.x : pixelSize.y;
-		var r = Std.int(ps * radius / dotSize);
+		var r = Std.int(ps * diameter / 2 / dotSize);
 		if (isDrawingEdge) {
 			for (er in r - edgeWidth + 1...r + 1) setOneCircle(er, true);
 		} else {
@@ -291,7 +392,7 @@ class D { // DotShape
 	public function pixelFillRect(x:Int, y:Int, width:Int, height:Int, c:C):Void {
 		setDot(x, y, 0);
 	}
-	function setDot(x:Int, y:Int, ry:Float):Void {
+	function setDot(x:Int, y:Int, ry:Float):OffsetColor {
 		var ca = (x * xSpotInterval).cos() * (y * ySpotInterval).cos() *
 			((x + y) * xySpotInterval).cos();
 		var c:C;
@@ -302,16 +403,17 @@ class D { // DotShape
 			if (colorBottom != null) c = color.bl(colorBottom, ry);
 			else c = color;
 		}
-		if (c.r < 0) return;
+		if (c.r < 0) return null;
 		var d = new OffsetColor();
 		d.offset.x = x + offset.x;
 		d.offset.y = y + offset.y;
 		d.color = c;
 		dots.push(d);
+		return d;
 	}
 	function setGeneratedColors(color:C, seed:Int):Void {
-		var r = R.i;
-		if (seed >= 0) r.s(seed);
+		if (seed < 0) seed = baseRandomSeed++;
+		var r = R.i.s(seed);
 		var cbl = C.di.v(color);
 		blinkColor(cbl, r);
 		c(cbl).cb(cbl.gd);
@@ -319,9 +421,9 @@ class D { // DotShape
 		cs(cbl).cbs(cbl.gd).si(r.ni(3), r.ni(3), r.ni(3));
 	}
 	function blinkColor(c:C, r:R):Void {
-		c.r += r.pi(80);
-		c.g += r.pi(80);
-		c.b += r.pi(80);
+		c.r += r.pi(64);
+		c.g += r.pi(64);
+		c.b += r.pi(64);
 		c.normalize();
 	}
 }
